7b891586be7c78225e9d1fe056316837d07532f4
Mehr-un-nisa <55224607+Mehr-un-nisa@users.noreply.github.com>
2022-03-19 15:37:08 +0500

    Firebase backend set

---
+++ Chatter-App/lib/pages/signup.dart
@@ -0,0 +1,182 @@
+import 'package:chat_app/widgets/custombutton.dart';
+import 'package:chat_app/widgets/customtextinput.dart';
+import 'package:edge_alert/edge_alert.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
+
+class ChatterSignUp extends StatefulWidget {
+  @override
+  _ChatterSignUpState createState() => _ChatterSignUpState();
+}
+
+class _ChatterSignUpState extends State<ChatterSignUp> {
+  final _auth = FirebaseAuth.instance;
+  String email;
+  // String username;
+  String password;
+  String name;
+  bool signingup = false;
+  @override
+  Widget build(BuildContext context) {
+    return ModalProgressHUD(
+      inAsyncCall: signingup,
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Container(
+            height: MediaQuery.of(context).size.height,
+            // margin: EdgeInsets.only(top:MediaQuery.of(context).size.height*0.2),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Image.asset('assets/images/VEXPOcd_logo.png'),
+
+
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.02,
+                  ),
+                  Hero(
+                    tag: 'HeroTitle',
+                    child: Text(
+                      'VEXPOcd',
+                      style: TextStyle(
+                          color: Colors.indigo[900],
+                          fontFamily: 'Poppins',
+                          fontSize: 26,
+                          fontWeight: FontWeight.w700),
+                    ),
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.01,
+                  ),
+                  // Text(
+                  //   "World's most private chatting app".toUpperCase(),
+                  //   style: TextStyle(
+                  //       fontFamily: 'Poppins',
+                  //       fontSize: 12,
+                  //       color: Colors.deepPurple),
+                  // ),
+                  CustomTextInput(
+                    hintText: 'Username',
+                    leading: Icons.text_format,
+                    obscure: false,
+                    userTyped: (value) {
+                      name = value;
+                    },
+                  ),
+                  SizedBox(
+                    height: 0,
+                  ),
+                  // CustomTextInput(
+                  //   hintText: 'Username',
+                  //   obscure: false,
+                  //   leading: Icons.supervised_user_circle,
+                  //   userTyped: (value) {
+                  //     username = value;
+                  //   },
+                  // ),
+                  SizedBox(
+                    height: 0,
+                  ),
+                  CustomTextInput(
+                    hintText: 'Email',
+                    leading: Icons.mail,
+                    keyboard: TextInputType.emailAddress,
+                    obscure: false,
+                    userTyped: (value) {
+                      email = value;
+                    },
+                  ),
+                  SizedBox(
+                    height: 0,
+                  ),
+                  CustomTextInput(
+                    hintText: 'Password',
+                    leading: Icons.lock,
+                    keyboard: TextInputType.visiblePassword,
+                    obscure: true,
+                    userTyped: (value) {
+                      password = value;
+                    },
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Hero(
+                    tag: 'signupbutton',
+                    child: CustomButton(
+                      mainColor: Colors.indigo[900],
+                      onpress: () async {
+                        if (name != null && password != null && email != null) {
+                          setState(() {
+                            signingup = true;
+                          });
+                          try {
+                            final newUser =
+                                await _auth.createUserWithEmailAndPassword(
+                                    email: email, password: password);
+                            if (newUser != null) {
+                              UserUpdateInfo info = UserUpdateInfo();
+                              info.displayName = name;
+                              await newUser.user.updateProfile(info);
+
+                              Navigator.pushNamed(context, '/chat');
+                            }
+                          } catch (e) {
+                            setState(() {
+                              signingup = false;
+                            });
+                            EdgeAlert.show(context,
+                                title: 'Signup Failed',
+                                description: e.toString(),
+                                gravity: EdgeAlert.BOTTOM,
+                                icon: Icons.error,
+                                backgroundColor: Colors.indigo[900]);
+                          }
+                        } else {
+                          EdgeAlert.show(context,
+                              title: 'Signup Failed',
+                              description: 'All fields are required.',
+                              gravity: EdgeAlert.BOTTOM,
+                              icon: Icons.error,
+                              backgroundColor: Colors.indigo[900]);
+                        }
+                      },
+                      text: 'sign up',
+                      accentColor: Colors.white,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  GestureDetector(
+                      onTap: () {
+                        Navigator.pushReplacementNamed(context, '/login');
+                      },
+                      child: Text(
+                        'or log in instead',
+                        style: TextStyle(
+                            fontFamily: 'Poppins',
+                            fontSize: 12,
+                            color: Colors.indigo),
+                      )),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.1,
+                  ),
+                  // Hero(
+                  //     tag: 'footer',
+                  //     child: Text(
+
+                  //       style: TextStyle(fontFamily: 'Poppins'),
+                  //     ))
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ Chatter-App/lib/pages/splash.dart
@@ -0,0 +1,115 @@
+import 'package:chat_app/widgets/custombutton.dart';
+import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+
+class ChatterHome extends StatefulWidget {
+  @override
+  _ChatterHomeState createState() => _ChatterHomeState();
+}
+
+class _ChatterHomeState extends State<ChatterHome>
+    with TickerProviderStateMixin {
+  AnimationController mainController;
+  Animation mainAnimation;
+  @override
+  void initState() {
+    super.initState();
+    mainController = AnimationController(
+      duration: Duration(milliseconds: 1700),
+      vsync: this,
+    );
+    mainAnimation = ColorTween(begin: Colors.indigo[900], end: Colors.grey[100])
+        .animate(mainController);
+    mainController.forward();
+    mainController.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: mainAnimation.value,
+      body: SafeArea(
+        child: Container(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Image.asset('assets/images/VEXPOcd_logo.png'),
+                SizedBox(height: 15),
+                // Hero(
+                //   tag: 'heroicon',
+                //   child: Icon(
+                //     Icons.textsms,
+                //     size: mainController.value * 100,
+                //     color: Colors.indigo[900],
+                //   ),
+                // ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.02,
+                ),
+                Hero(
+                  tag: 'HeroTitle',
+                  child: Text(
+                    'VEXPOcd',
+                    style: TextStyle(
+                        color: Colors.indigo[900],
+                        fontFamily: 'Poppins',
+                        fontSize: 26,
+                        fontWeight: FontWeight.w700),
+                  ),
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.01,
+                ),
+                TyperAnimatedTextKit(
+                  isRepeatingAnimation: false,
+                  speed: Duration(milliseconds: 60),
+                  text: [
+                    "OCD makes life difficut but you can still enjoy a happy life"
+                  ],
+                  textStyle: TextStyle(
+                      fontFamily: 'Poppins',
+                      fontSize: 12,
+                      color: Colors.indigo[900]),
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.15,
+                ),
+                Hero(
+                  tag: 'loginbutton',
+                  child: CustomButton(
+                    text: 'Login',
+                    accentColor: Colors.indigo[900],
+                    onpress: () {
+                      Navigator.pushReplacementNamed(context, '/login');
+                    },
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Hero(
+                  tag: 'signupbutton',
+                  child: CustomButton(
+                    text: 'Signup',
+                    accentColor: Colors.white,
+                    mainColor: Colors.indigo[900],
+                    onpress: () {
+                      Navigator.pushReplacementNamed(context, '/signup');
+                    },
+                  ),
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.1,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
